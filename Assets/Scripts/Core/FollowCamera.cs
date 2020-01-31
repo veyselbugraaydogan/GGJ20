8da76af145cdf1e76ae88865941bca224c5eafde
Veysel Aydoğan <veyselbugraaydogan@gmail.com>
2020-01-31 21:39:19 +0300

    Structured

---
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    
+
+    
+    void LateUpdate()
+    {
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerControl : MonoBehaviour
+{
+    NavMeshAgent player;
+    [SerializeField]Transform target;
+
+    void Start()
+    {
+        player = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player.SetDestination(target.position);
+    }
+}
