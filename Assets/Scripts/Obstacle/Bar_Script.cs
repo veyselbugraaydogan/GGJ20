9c3649a541aee52ad7875f6bf01dfbde4373d2ac
Veysel Aydoğan <veyselbugraaydogan@gmail.com>
2020-02-01 13:24:00 +0300

    Implement FillBar

---
+++ Assets/Scripts/Obstacle/Bar_Script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Bar_Script : MonoBehaviour
+{
+    public Image Bar;
+    public float Fill;
+	public bool boolean=true;
+
+    void Start()
+    {
+		Bar.fillAmount=0;
+    }
+	
+    void Update()
+    {
+		if(Input.GetMouseButtonDown(0))
+		{
+        Bar.fillAmount+=Fill;
+		}
+		
+	
+    }
+	
+	
+}
